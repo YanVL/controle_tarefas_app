f5766c7677317ac726531dd826426cd3b7977f44
YanVl <yanvitor.gti@gmail.com>
2022-11-16 06:40:38 -0300

    Redefinicao de senha e traducoes

---
+++ app/Notifications/RedefinirSenhaNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
+
+class RedefinirSenhaNotification extends Notification
+{
+    use Queueable;
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = 'http://127.0.0.1:8000/password/reset/'.$this->token;
+        $minutos = config('auth.passwords.' . config('auth.defaults.passwords') . '.expire');
+        return (new MailMessage)
+            ->subject('Atualização de senha')
+            ->line('Você está recebendo esse e-mail pois solicitou uma redefinição de senha')
+            ->action('Clique aqui para modificar a senha', $url)
+            ->line('O link para redefinição expira em '.$minutos. 'minutos.')
+            ->line('Se você não solicitou essa redefinição de senha, por favor, ignore este e-mail.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Mail\MensagemTesteMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +23,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa', App\Http\Controllers\TarefaController::class);
+
 Route::get('/mensagem-teste', function () {
     return new MensagemTesteMail();
+    //Mail::to('ycontroledtarefas@gmail.com')->send(new MensagemTesteMail());
+    //return 'E-mail enviado com sucesso';
 });
